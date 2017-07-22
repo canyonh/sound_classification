c5e5bd8c18f91981de3140d2352e26fa5fed59c4
Kun-Yao <canyonh@gmail.com>
2017-07-22 10:25:05 +1000

    add requirement for pyset

--- pyenv-setup/setup-python.sh
+++ pyenv-setup/setup-python.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install -y gfortran libopenblas-dev liblapack-dev libfreetype6-dev libpng-dev libjpeg-dev pkg-config libncurses5-dev python-tk python-pip
 
-pip install --upgrade --user Cython Jinja2 MarkupSafe Pillow Pygments appnope argparse backports-abc backports.ssl-match-hostname certifi cycler decorator future gnureadline ipykernel ipython ipython-genutils ipywidgets jsonschema jupyter jupyter-client jupyter-console jupyter-core matplotlib mistune nbconvert nbformat notebook numpy path.py pexpect pickleshare ptyprocess pyparsing python-dateutil pytz pyzmq qtconsole scipy simplegeneric singledispatch site six terminado tornado traitlets librosa
-
+#pip install --upgrade --user Cython Jinja2 MarkupSafe Pillow Pygments appnope argparse backports-abc backports.ssl-match-hostname certifi cycler decorator future gnureadline ipykernel ipython ipython-genutils ipywidgets jsonschema jupyter jupyter-client jupyter-console jupyter-core matplotlib mistune nbconvert nbformat notebook numpy path.py pexpect pickleshare ptyprocess pyparsing python-dateutil pytz pyzmq qtconsole scipy simplegeneric singledispatch site six terminado tornado traitlets librosa
+pip install -r requirements.txt

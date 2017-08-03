774ee9b96f14280a06be6afe5d6e562e9bb2dd5d
kxhuan <kxhuan@dolby.com>
2017-08-04 09:17:50 +1000

    python packages installed to current user

--- pyenv-setup/setup-python.sh
+++ pyenv-setup/setup-python.sh
@@ -7,4 +7,4 @@ sudo apt-get install -y ffmpeg
 
 
 #pip install --upgrade --user Cython Jinja2 MarkupSafe Pillow Pygments appnope argparse backports-abc backports.ssl-match-hostname certifi cycler decorator future gnureadline ipykernel ipython ipython-genutils ipywidgets jsonschema jupyter jupyter-client jupyter-console jupyter-core matplotlib mistune nbconvert nbformat notebook numpy path.py pexpect pickleshare ptyprocess pyparsing python-dateutil pytz pyzmq qtconsole scipy simplegeneric singledispatch site six terminado tornado traitlets librosa
-pip install -r requirements.txt
+pip install --user -r requirements.txt

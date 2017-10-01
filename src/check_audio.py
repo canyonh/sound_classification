6254aad29fade992d8d4de3ff327fb7c1d25483a
Kun-Yao <canyonh@gmail.com>
2017-10-01 22:53:49 +1100

    fix issue found by syntastic

--- src/check_audio.py
+++ src/check_audio.py
@@ -2,10 +2,10 @@ import os
 import sys
 import glob
 import librosa
-import random
 import numpy as np
 import logging
 from random import shuffle
+
 
 class DataSet:
 
@@ -18,7 +18,7 @@ class DataSet:
     def Load(self, path):
         files = glob.glob(os.path.join(path, "*"))
         shuffle(files)
-        
+
         correct_classes = []
         for f in files:
             logging.debug("Loading file %s", f)
@@ -40,7 +40,7 @@ class DataSet:
             idx = self.labels.index(token)
         except ValueError:
             pass
-        
+
         if idx == -1:
             correct_class = len(self.labels)
             self.labels.append(token)
@@ -48,8 +48,6 @@ class DataSet:
         else:
             correct_class = idx
             logging.debug("adding a new label %s for file %s, class: %d", token, base_name, correct_class)
-        
-        return correct_class
 
     def LoadImpl(self, path):
         raise NotImplementedError()
@@ -59,34 +57,38 @@ class DataSet:
         num_classes = len(self.labels)
         one_hot = np.zeros(num_classes)
         one_hot[correct_class] = 1
-        #print("total: ", num_classes, "class: ", correct_class, "one hot: ", one_hot)
+        # print("total: ", num_classes, "class: ", correct_class, "one hot: ", one_hot)
         return one_hot
+
 
 class WaveDataSet(DataSet):
 
     def LoadImpl(self, path):
-        y, sr= librosa.load(path, 8000)
-        logging.debug("wave file %s loaded, sample rate:%d",path, sr)
+        y, sr = librosa.load(path, 8000)
+        logging.debug("wave file %s loaded, sample rate:%d", path, sr)
         return y
 
 #
 # conversion
 #
 
+
 # resample in-place
 def Resample(filename, target_sampling_rate):
     y, _ = librosa.load(filename, sr=target_sampling_rate)
-    librosa.output.write_wav(filename, y, target_sample_rate)
+    librosa.output.write_wav(filename, y, target_sampling_rate)
+
 
 # generate spectrogram
 def GenerateSpectrogram(src_filename, target_filename, sampling_rate, num_fft=320, hop_len=160, num_mels=80, flatten=True):
     logging.debug("GenerateSpectrogram(), src file: %s, dst file: %s", src_filename, target_filename)
     y, _ = librosa.core.load(src_filename, sr=sampling_rate)
     spectro = librosa.feature.melspectrogram(y, sr=sampling_rate, n_fft=num_fft, hop_length=hop_len, n_mels=num_mels)
-    if flatten == True:
-        np.save(target_sampling_rate, spectro.flatten())
+    if flatten is True:
+        np.save(target_filename, spectro.flatten())
     else:
         np.save(target_filename, spectro)
+
 
 def ResampleDir(target_dir):
     files = glob.glob(os.path.join(target_dir, ".*"))
@@ -94,8 +96,10 @@ def ResampleDir(target_dir):
         logging.debug("resample %s to 8000 HZ", f)
         Resample(f, 8000)
 
+
 def RootDir():
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+
 
 def ShowLog():
     root = logging.getLogger()
@@ -104,22 +108,25 @@ def ShowLog():
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     root.addHandler(ch)
-   
+
+
 #
 # Tests
 #
 def TestLoadSample():
-    dir_path = os.path.join(RootDir(),"data/systest-prototype-small")
+    dir_path = os.path.join(RootDir(), "data/systest-prototype-small")
     wave_data_set = WaveDataSet()
     wave_data_set.Load(dir_path)
-   
+
+
 def main():
     ShowLog()
     # only need once
-    #ResampleDir(os.path.join(RootDir(), "data/systest-prototype-small"))
+    # ResampleDir(os.path.join(RootDir(), "data/systest-prototype-small"))
     TestLoadSample()
-    
+
     return
+
 
 if __name__ == "__main__":
     main()

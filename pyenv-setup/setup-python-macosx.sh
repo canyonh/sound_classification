551fb67906e4b09f3008697a05338bee01046a99
kxhuan <kxhuan@dolby.com>
2017-10-10 12:43:28 +1100

    add script to setup python on mac

---
+++ pyenv-setup/setup-python-macosx.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools
+
+pip install -r requirements.txt

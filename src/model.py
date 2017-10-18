43746cf3fa08fd20a47a4f746531fc44b2ea381d
Kun-Yao <canyonh@gmail.com>
2017-10-18 22:41:35 +1100

    fix session life time issue; infer still not working

--- src/model.py
+++ src/model.py
@@ -10,8 +10,11 @@ def TestLinearModel():
     dir_path = os.path.join(common.DataDir(), "systest-prototype-small-npy")
     print(dir_path)
     test_dataset.Load(dir_path)
-    linear_model = LinearModel()
-    linear_model.Train(test_dataset, 10, 1)
+    with LinearModel() as linear_model:
+        linear_model.Train(test_dataset, 1000, 1)
+        infer_result = linear_model.Infer(test_dataset.x)
+        for result in infer_result:
+            logging.info("infer result: %s", str(result))
 
 
 class LinearModel:
@@ -22,6 +25,13 @@ class LinearModel:
     def __init__(self):
         self.session = None
         return
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exec_type, exec_value, traceback):
+        if self.session is not None:
+            self.session.close()
 
     # @todo
     def Train(self, training_set, epoch_cnt,
@@ -59,26 +69,26 @@ class LinearModel:
 
         self.session = tf.Session()
         # @todo session lifetime management
-        with tf.Session() as sess:
-            num_train = len(training_set.x)
-            tf.global_variables_initializer().run()
+        num_train = len(training_set.x)
+        self.session.run(tf.global_variables_initializer())
 
-            # training
-            for epoch in range(epoch_cnt):
-                epoch_loss = 0
-                current = 0
-                for _ in range(int(num_train/batch_size)):
-                    c, _ = sess.run([self.cost, self.optimizer],
-                                    feed_dict={self.x: training_set.x,
-                                    self.y_correct: training_set.y})
-                    epoch_loss += c
-                current += batch_size
-                logging.info("Epoch: %d, loss: %f", epoch, epoch_loss)
-            correct_prediction = tf.equal(tf.argmax(self.y_output, 1),
-                                          tf.argmax(self.y_correct, 1))
-            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction,
-                                           tf.float32))
-            logging.info("Accuracy: %s", str(self.accuracy))
+        # training
+        for epoch in range(epoch_cnt):
+            epoch_loss = 0
+            current = 0
+            for _ in range(int(num_train/batch_size)):
+                c, _ = self.session.run([self.cost, self.optimizer],
+                                        feed_dict={self.x: training_set.x,
+                                        self.y_correct: training_set.y})
+                epoch_loss += c
+            current += batch_size
+            logging.info("Epoch: %d, loss: %f", epoch, epoch_loss)
+        correct_prediction = tf.equal(tf.argmax(self.y_output, 1),
+                                      tf.argmax(self.y_correct, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction,
+                                       tf.float32))
+        logging.info("Accuracy: %s", str(self.accuracy))
 
-    def Infer(self):
-        raise NotImplementedError
+    def Infer(self, x):
+        result = self.session.run([self.y_output], feed_dict={self.x: x})
+        return tf.argmax(result, 1)
